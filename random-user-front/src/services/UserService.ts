b3e0975eaaa051e8d4a8eadd4bb0546e5229328e
Rafa Linares <rafa.linares.molina@gmail.com>
2023-06-12 05:22:33 +0200

    refactor: added error handle on service

--- random-user-front/src/services/UserService.ts
+++ random-user-front/src/services/UserService.ts
@@ -1,9 +1,19 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { User } from '../interfaces/User';
+
+interface APIResponse {
+  results: User[];
+}
 
 export class UserService {
   static async getRandomUsers(): Promise<User[]> {
-    const response = await axios.get('https://randomuser.me/api/?results=50');
-    return response.data.results;
+    try {
+      const response: AxiosResponse<APIResponse> = await axios.get('https://randomuser.me/api/?results=20');
+      return response.data.results;
+
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 }

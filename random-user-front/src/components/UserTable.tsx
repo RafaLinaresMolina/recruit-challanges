27fea036a1c5f54838512986a2cba379a6daf544
Rafa Linares <rafa.linares.molina@gmail.com>
2023-06-12 13:55:02 +0200

    feature: added drag and drop, also improved csv management

--- random-user-front/src/components/UserTable.tsx
+++ random-user-front/src/components/UserTable.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState, ChangeEvent } from 'react';
 import { SearchUser, User } from '../interfaces/User';
 import { UserService } from '../services/UserService';
+import { CSVService } from '../services/CSVService';
+import { DragDropContext, Draggable, DropResult, Droppable } from 'react-beautiful-dnd';
 import styles from './UserTable.module.scss';
 
 
@@ -56,18 +58,65 @@ const UserRow: React.FC<UserRowProps> = ({ user, handleSelect, selected }) => (
     </tr>
 );
 
+const DraggableUserRow: React.FC<UserRowProps & { index: number }> = ({ user, handleSelect, selected, index }) => (
+    <Draggable draggableId={user.email} index={index}>
+        {(provided) => (
+            <tr
+                ref={provided.innerRef}
+                {...provided.draggableProps}
+                {...provided.dragHandleProps}
+                className={selected ? styles.selected : ''}
+            >
+                <td>
+                    <input type="checkbox" onChange={() => handleSelect(user)} checked={selected} />
+                </td>
+                <td>{user.name.first} {user.name.last}</td>
+                <td>{user.gender}</td>
+                <td>{user.email}</td>
+            </tr>
+        )}
+    </Draggable>
+);
+
 const UserTable = () => {
     const [users, setUsers] = useState<User[]>([]);
     const [searchTerms, setSearchTerms] = useState<SearchUser>({ name: '', gender: '', email: '' });
     const [sort, setSort] = useState<{ field: keyof User | '', order: SortOrder.ASC | SortOrder.DESC }>({ field: '', order: SortOrder.ASC });
     const [selectedRows, setSelectedRows] = useState<User[]>([]);
     const [selectAll, setSelectAll] = useState(false);
+    const [dragedUsers, setDraggedUsers] = useState<User[]>([]);
+    const csvService = new CSVService();
+
+    const onDragEnd = (result: DropResult) => {
+        const { source, destination } = result;
+
+        if (!destination) {
+            return;
+        }
+
+        if (source?.droppableId !== destination.droppableId) {
+            const sourceList = source?.droppableId === "users" ? users : dragedUsers;
+            const destinationList = destination.droppableId === "users" ? users : dragedUsers;
+            const removed = sourceList[source.index];
+            sourceList.splice(source.index, 1);
+            destinationList.splice(destination.index, 0, removed);
+
+            if (source?.droppableId === "users") {
+                setUsers([...sourceList]);
+                setDraggedUsers([...destinationList]);
+            } else {
+                setUsers([...destinationList]);
+                setDraggedUsers([...sourceList]);
+            }
+        }
+    };
 
     useEffect(() => {
         UserService.getRandomUsers()
             .then(setUsers)
             .catch(console.error);
     }, []);
+
 
     const handleSearch = (e: React.ChangeEvent<HTMLInputElement>, field: keyof User) => {
         setSearchTerms({ ...searchTerms, [field]: e.target.value });
@@ -126,42 +175,124 @@ const UserTable = () => {
         filteredAndSortedUsers = filteredAndSortedUsers.sort(sortFunction);
     }
 
-    const exportToCSV = () => {
-        const toExport = selectedRows.length ? selectedRows : users;
-        const header = 'Name,Gender,Email\n';
-        const csv = toExport.map(user => `${user.name.first} ${user.name.last},${user.gender},${user.email}`).join('\n');
-        const csvWithHeader = header + csv;
-        const blob = new Blob([csvWithHeader], { type: 'text/csv;charset=utf-8;' });
+    const exportToCSV = (data: User[], headers: (keyof User)[], filename: string) => {
+        const flattenedData = data.map(user => ({
+            ...user,
+            name: `${user.name.first} ${user.name.last}`
+        }));
+        const csvContent = csvService.generateCSVContent(flattenedData, headers);
+        const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
         const url = URL.createObjectURL(blob);
         const link = document.createElement('a');
         link.href = url;
         const timestamp = Date.now();
-        link.setAttribute('download', `users_${timestamp}.csv`);
+        link.setAttribute('download', `${filename}_${timestamp}.csv`);
         link.click();
+    };
+
+    const exportSelectedToCSV = () => {
+        const headers: (keyof User)[] = ['name', 'gender', 'email'];
+        exportToCSV(selectedRows.length ? selectedRows : users, headers, 'users');
+    };
+
+    const exportDragedToCSV = () => {
+        const headers: (keyof User)[] = ['name', 'gender', 'email'];
+        exportToCSV(dragedUsers.length ? dragedUsers : users, headers, 'draged_users');
     };
 
     return (
         <>
-            <div className={styles.container}>
-                <button onClick={exportToCSV}>Export to CSV</button>
-                <table className={styles.table}>
-                    <thead>
-                        <tr>
-                            <th>
-                                <input type="checkbox" onChange={handleSelectAll} checked={selectAll} />
-                            </th>
-                            <UserHeader field='name' sort={sort} handleSort={handleSort} handleSearch={handleSearch} />
-                            <UserHeader field='gender' sort={sort} handleSort={handleSort} handleSearch={handleSearch} />
-                            <UserHeader field='email' sort={sort} handleSort={handleSort} handleSearch={handleSearch} />
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {filteredAndSortedUsers.map((user, index) => (
-                            <UserRow key={index} user={user} handleSelect={handleSelect} selected={selectedRows.includes(user)} />
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+            <DragDropContext onDragEnd={onDragEnd}>
+                <div className={styles.flexContainer}>
+                    <div className={styles.tableContainer}>
+                        <button onClick={exportSelectedToCSV}>Export to CSV</button>
+                        <Droppable droppableId="users">
+                            {(provided) => (
+                                <table
+                                    ref={provided.innerRef}
+                                    {...provided.droppableProps}
+                                    className={styles.table}
+                                >
+                                    <thead>
+                                        <tr>
+                                            <th>
+                                                <input
+                                                    type="checkbox"
+                                                    onChange={handleSelectAll}
+                                                    checked={selectAll}
+                                                />
+                                            </th>
+                                            <UserHeader
+                                                field="name"
+                                                sort={sort}
+                                                handleSort={handleSort}
+                                                handleSearch={handleSearch}
+                                            />
+                                            <UserHeader
+                                                field="gender"
+                                                sort={sort}
+                                                handleSort={handleSort}
+                                                handleSearch={handleSearch}
+                                            />
+                                            <UserHeader
+                                                field="email"
+                                                sort={sort}
+                                                handleSort={handleSort}
+                                                handleSearch={handleSearch}
+                                            />
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {filteredAndSortedUsers.map((user, index) => (
+                                            <DraggableUserRow
+                                                user={user}
+                                                handleSelect={handleSelect}
+                                                selected={selectedRows.includes(user)}
+                                                index={index}
+                                                key={user.email}
+                                            />
+                                        ))}
+                                        {provided.placeholder}
+                                    </tbody>
+                                </table>
+                            )}
+                        </Droppable>
+                    </div>
+                    <div className={styles.tableContainer}>
+                        <button onClick={() => exportDragedToCSV()}>
+                            Export dragged users to CSV
+                        </button>
+                        <Droppable droppableId="draggedUsers">
+                            {(provided) => (
+                                <table
+                                    ref={provided.innerRef}
+                                    {...provided.droppableProps}
+                                    className={styles.table}
+                                >
+                                    <thead>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Gender</th>
+                                            <th>Email</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {dragedUsers.map((user, index) => (
+                                            <UserRow
+                                                user={user}
+                                                handleSelect={handleSelect}
+                                                selected={selectedRows.includes(user)}
+                                                key={user.email}
+                                            />
+                                        ))}
+                                        {provided.placeholder}
+                                    </tbody>
+                                </table>
+                            )}
+                        </Droppable>
+                    </div>
+                </div>
+            </DragDropContext>
         </>
     );
 };
